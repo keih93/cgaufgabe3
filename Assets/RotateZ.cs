86e2b7bd8bf2240141fc787275486a9103bd3a80
Tatiyana Mozovka <tmozovka@gmail.com>
2019-12-16 09:22:12 +0100

    Create Projekt

---
+++ Assets/RotateZ.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateZ : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 0f, 100f) * Time.deltaTime);
+    }
+}
+
